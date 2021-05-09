4f1bf8b104afe933a7e7846f2e3bfd7fdc93938d
JJKOrlino <jjkorlino80@yahoo.com>
2021-05-08 22:24:22 -0500

    added components

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
+//importing components
 import Form from './components/Form';
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <h1>Flatiron's Todo List</h1>
-      <Form />
-      <TodoList />
+      <h1>Flatiron's Todo List </h1>
+      {/* rendering these being imported. setInputText is prop */}
+      <Form 
+        inputText={inputText}
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText}/>
+      <TodoList todos={todos} />
     </div>
   );
 }
